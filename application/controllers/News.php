16a3d0379729b9a5efcdc0a2fe97210c441a9827
JosephWanderer <banhstopjw@gmail.com>
2017-04-25 05:50:46 -0700

    adds themes to CI application

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -14,9 +14,9 @@ class News extends CI_Controller {
         $data['news'] = $this->news_model->get_news();
         $data['title'] = 'News archive';
 
-        $this->load->view('templates/header', $data);
+        //$this->load->view('templates/header', $data);
         $this->load->view('news/index', $data);
-        $this->load->view('templates/footer');
+        //$this->load->view('templates/footer');
     }
 
     public function view($slug = NULL)
@@ -30,9 +30,9 @@ class News extends CI_Controller {
 
         $data['title'] = $data['news_item']['title'];
 
-        $this->load->view('templates/header', $data);
+        //$this->load->view('templates/header', $data);
         $this->load->view('news/view', $data);
-        $this->load->view('templates/footer', $data);
+        //$this->load->view('templates/footer', $data);
     }
     
     public function create()
@@ -47,17 +47,17 @@ class News extends CI_Controller {
 
         if ($this->form_validation->run() === FALSE)
         {//show form
-            $this->load->view('templates/header', $data);
+            //$this->load->view('templates/header', $data);
             $this->load->view('news/create', $data);
-            $this->load->view('templates/footer', $data);
+            //$this->load->view('templates/footer', $data);
 
         }
         else
         {// say thanks for entering data!
             $this->news_model->set_news();
-            $this->load->view('templates/header', $data);
+            //$this->load->view('templates/header', $data);
             $this->load->view('news/success', $data);
-            $this->load->view('templates/footer', $data);
+            //$this->load->view('templates/footer', $data);
         }
             
     }//end of create method
